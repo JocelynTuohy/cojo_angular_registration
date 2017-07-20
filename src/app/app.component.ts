dd8749b9e58fb5767abe0207ab14fce3aa3ef753
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-07-20 12:50:15 -0700

    completed registration assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Registration'
+  states: Array<string> = ['Alabama','Alaska','American Samoa','Arizona','Arkansas',
+    'California','Colorado','Connecticut','Delaware','District of Columbia',
+    'Federated States of Micronesia','Florida','Georgia','Guam','Hawaii',
+    'Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine',
+    'Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota',
+    'Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire',
+    'New Jersey','New Mexico','New York','North Carolina','North Dakota',
+    'Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau',
+    'Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota',
+    'Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia',
+    'Washington','West Virginia','Wisconsin','Wyoming']
+  succeeded: boolean = false;
+  user: object = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirm: '',
+    addr1: '',
+    addr2: '',
+    city: '',
+    state: '',
+    lucky: undefined,
+  }
+  constructor(){}
+  register(){
+    this.succeeded = true;
+  }
 }
